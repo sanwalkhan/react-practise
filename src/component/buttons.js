12542122a1395e4f47ec9df2eeef09a812224fe5
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2021-09-09 10:36:09 +0500

    cards done

--- src/component/buttons.js
+++ src/component/buttons.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-export const Buttons = ({category}) => {
+export const Buttons = ({category ,fun} ) => {
 
     // console.log(category);
+    // console.log(fun)
 
 
 
     return (
         <div>
-            {category.map((i,j)=><button className="btn btn-success" type="button" key={j} onClick={()=>console.log("hello")}>{i}</button>)}
+            {category.map((i,j)=><button className="btn btn-success" type="button" key={j} onClick={()=> fun(i)}>{i}</button>)}
         </div>
     )
 }
