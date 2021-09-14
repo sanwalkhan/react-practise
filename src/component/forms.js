0f383b7d5f454812cc3f74a250b8c6fdbb42b026
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2021-09-14 14:47:14 +0500

    Update forms.js

--- src/component/forms.js
+++ src/component/forms.js
@@ -5,29 +5,34 @@ export const Form = () => {
   const [list, setlist] = useState([]);
 
   const onclickhandler = () => {
-    setlist([...list, name]);
-    setname("");
+    setlist([...list,{id: list.length , task : name}]);
+    setname(' ')
+
   };
+
+
+  const DeleteItem =(id)=>{
+
+    setlist(list.filter(name => name.id !== id))
+
+    console.log("Delete");
+  }
 
   return (
     <div style={{ textAlign: "center", background: "#990", padding: "100px" }}>
-     
-
-      <h1> {name} </h1>
-
+      <h1> Todos App </h1>
       <input
         type="text"
         value={name}
         onChange={(e) => setname(e.target.value)}
         placeholder="Please Enter Your Name"
       />
-
       <button onClick={onclickhandler}>Add Task</button>
-
       {list.map((itemlist, i) => (
-        <h3 key={i}> {itemlist} </h3>
+        <h3 key={i}> {itemlist.task}
+        <button onClick={()=>DeleteItem(i)} >Delete</button>
+         </h3>
       ))}
-    
     </div>
   );
 };
