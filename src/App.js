626d6c33c3ec93ac469474722d8f24dfd5dd262e
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2021-09-29 10:52:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import { useEffect, useState } from "react";
 
 function App() { 
 
-  const [num, setnum] = useState(0)
-  const [nam, setnam] = useState(10)
+  // const [num, setnum] = useState(0)
+  // const [nam, setnam] = useState(10)
 
   useEffect(() => {
     console.log("Useeffect..................");
-  }, [nam] )
+  } )
 
-  useEffect(() =>{
-    console.log("Useeffecttttt");
-  } , [num] )
+  // useEffect(() =>{
+    // console.log("Useeffecttttt");
+  // } , [num] )
     return (
       <div>
-        <h1> {num} </h1>
-        <h1>
-          {nam}
-        </h1>
-      <button onClick={ ()=> setnum(num + 1) } >Btn</button>
+        {console.log("renderr....")}
+        {/* <h1> {num} </h1> */}
+        {/* <h1> */}
+          {/* {nam} */}
+        {/* </h1> */}
+      {/* <button onClick={ ()=> setnum(num + 1) } >Btn</button> */}
 
-      <button onClick={ ()=> setnam(nam - 1) } >Btn2</button>
+      {/* <button onClick={ ()=> setnam(nam - 1) } >Btn2</button> */}
 
       </div>
     
@@ -30,3 +31,7 @@ function App() {
 
 
  export default App
+
+
+
+//  usseeffect
