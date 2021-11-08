d3fae40a27101c836ed9cb5e7f652a2e0dc81ba9
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2021-11-09 00:28:13 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
-import { useState } from 'react';
-// import './App.css';
-// import { State } from "./component/statefunctionalcomponent";
-
-import { Child } from "./component/childdidmount";
-// import { List } from "./component/list";
-
-// import { SlideBar } from "./component/slidebar";
-
-// import Counter from "./component/ecounter";
-
-// import MyComponent from "./component/stateclasscomponents";
-
-// import State from './component/state'
-
-// import Cards from './component/cards';
-// import Header from "./component/Header"
-
-// import ClassComponent from './component/classcomponent';
-
-// import propTypes from 'prop-types';
-
-// let a = 444;
-
-// function ca() {
-//   a =999;
-//   alert(a)
-//  }
+import React from 'react';
 
 function App() {
-  // const email = "@yahoo.com";
-  // const name = 'sanwal';
 
-  // function showname(){
-  // alert(name)
-  // }
-
-  // const object = {name  , email , showname}
-
-  // const  arr = ["lahore" , "multan " , "fsd", "sgd" , {object} ]
-
-  // const [a, seta] = useState(444444)
-
-  // function ca(){
-  //   seta(1242)
-  // }
-
-  const [count, setcount] = useState(0)
 
   return (
-    //   <div>
-    //     <ClassComponent arr={arr} />
-
-    // {/* <Header/> */}
-
-    // {/* <Cards/>  */}
-    //   </div>
-
-    // <div className="App">
-    //  <h1>{a}</h1>
-    //  <button onClick={ca}>btn</button>
-    // </div>
-
+   
     <div>
-      {/* <Counter /> */}
-     
-    <button onClick={()=>setcount(count+1)}>BTN</button>
-      <Child count = {count}/> 
-
-    {/* <List/> */}
-
-    {/* <SlideBar /> */}
-
-    {/* <MyComponent /> */}
-
-    {/* <State /> */}
-
+      <h1>Sanwal Khan</h1>
     </div>
+   
+    
   );
 }
 
-// App.prototype = {
-// name: propTypes.number.isRequired
-
-// }
 
 export default App;
