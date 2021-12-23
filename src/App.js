c855eaae376aa6eb45da7c80817d6766c3ab3194
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2021-12-23 21:32:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Component } from 'react';
 import './App.css';
-import Cards from './components/cards'
+// import Cards from './components/cards'
 
 class App extends Component{
+
+  
+
   render(){
     return(
-    <div>
-      <Cards />
+    <div className = 'App'>
+     
+     Sanwal Khan
+
     </div>
     )
   }
